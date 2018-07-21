6f396834a2c47314d9daff6fb18e4080bd63b60e
dmitry.dygalo <dadygalo@gmail.com>
2018-07-21 20:26:51 +0200

    Lint

--- xdump/postgresql.py
+++ xdump/postgresql.py
@@ -47,11 +47,18 @@ FROM
     pg_namespace nr,
     pg_constraint c,
     pg_class r
-  WHERE nc.oid = c.connamespace AND nr.oid = r.relnamespace AND c.conrelid = r.oid AND (c.contype <> ALL (ARRAY['t'::"char", 'x'::"char"])) AND r.relkind = 'r'::"char" AND NOT pg_is_other_temp_schema(nr.oid)
+  WHERE 
+    nc.oid = c.connamespace AND 
+    nr.oid = r.relnamespace AND 
+    c.conrelid = r.oid AND 
+    (c.contype <> ALL (ARRAY['t'::"char", 'x'::"char"])) AND 
+    r.relkind = 'r'::"char" AND NOT pg_is_other_temp_schema(nr.oid)
 UNION ALL
  SELECT current_database()::information_schema.sql_identifier AS constraint_catalog,
     nr.nspname::information_schema.sql_identifier AS constraint_schema,
-    (((((nr.oid::text || '_'::text) || r.oid::text) || '_'::text) || a.attnum::text) || '_not_null'::text)::information_schema.sql_identifier AS constraint_name,
+    (
+      ((((nr.oid::text || '_'::text) || r.oid::text) || '_'::text) || a.attnum::text) || '_not_null'::text
+    )::information_schema.sql_identifier AS constraint_name,
     current_database()::information_schema.sql_identifier AS table_catalog,
     nr.nspname::information_schema.sql_identifier AS table_schema,
     r.relname::information_schema.sql_identifier AS table_name,
@@ -61,7 +68,14 @@ UNION ALL
    FROM pg_namespace nr,
     pg_class r,
     pg_attribute a
-  WHERE nr.oid = r.relnamespace AND r.oid = a.attrelid AND a.attnotnull AND a.attnum > 0 AND NOT a.attisdropped AND r.relkind = 'r'::"char" AND NOT pg_is_other_temp_schema(nr.oid)
+  WHERE 
+    nr.oid = r.relnamespace AND 
+    r.oid = a.attrelid AND 
+    a.attnotnull AND 
+    a.attnum > 0 AND 
+    NOT a.attisdropped AND 
+    r.relkind = 'r'::"char" AND 
+    NOT pg_is_other_temp_schema(nr.oid)
     ) AS tc
     JOIN information_schema.key_column_usage AS kcu
       ON tc.constraint_name = kcu.constraint_name AND kcu.table_name = tc.table_name
@@ -85,7 +99,18 @@ UNION ALL
             pg_depend d,
             pg_namespace nc,
             pg_constraint c
-          WHERE nr.oid = r.relnamespace AND r.oid = a.attrelid AND d.refclassid = 'pg_class'::regclass::oid AND d.refobjid = r.oid AND d.refobjsubid = a.attnum AND d.classid = 'pg_constraint'::regclass::oid AND d.objid = c.oid AND c.connamespace = nc.oid AND c.contype = 'c'::"char" AND r.relkind = 'r'::"char" AND NOT a.attisdropped
+          WHERE 
+            nr.oid = r.relnamespace AND 
+            r.oid = a.attrelid AND 
+            d.refclassid = 'pg_class'::regclass::oid AND
+            d.refobjid = r.oid AND 
+            d.refobjsubid = a.attnum AND 
+            d.classid = 'pg_constraint'::regclass::oid AND 
+            d.objid = c.oid AND 
+            c.connamespace = nc.oid AND 
+            c.contype = 'c'::"char" AND 
+            r.relkind = 'r'::"char" AND 
+            NOT a.attisdropped
         UNION ALL
          SELECT nr.nspname,
             r.relname,
@@ -98,11 +123,22 @@ UNION ALL
             pg_attribute a,
             pg_namespace nc,
             pg_constraint c
-          WHERE nr.oid = r.relnamespace AND r.oid = a.attrelid AND nc.oid = c.connamespace AND
-                CASE
-                    WHEN c.contype = 'f'::"char" THEN r.oid = c.confrelid AND (a.attnum = ANY (c.confkey))
-                    ELSE r.oid = c.conrelid AND (a.attnum = ANY (c.conkey))
-                END AND NOT a.attisdropped AND (c.contype = ANY (ARRAY['p'::"char", 'u'::"char", 'f'::"char"])) AND r.relkind = 'r'::"char") x(tblschema, tblname, tblowner, colname, cstrschema, cstrname)
+          WHERE 
+            nr.oid = r.relnamespace AND 
+            r.oid = a.attrelid AND 
+            nc.oid = c.connamespace AND
+            CASE
+              WHEN c.contype = 'f'::"char" 
+                THEN 
+                  r.oid = c.confrelid AND (a.attnum = ANY (c.confkey))
+                ELSE 
+                  r.oid = c.conrelid AND (a.attnum = ANY (c.conkey))
+            END AND 
+            NOT a.attisdropped AND 
+            (
+              c.contype = ANY (ARRAY['p'::"char", 'u'::"char", 'f'::"char"])) AND 
+              r.relkind = 'r'::"char"
+            ) x(tblschema, tblname, tblowner, colname, cstrschema, cstrname)
     ) AS ccu
       ON ccu.constraint_name = tc.constraint_name
 WHERE
