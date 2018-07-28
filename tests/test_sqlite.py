f20f0e5398b914522ca5d11f93e23f27fb73904c
dmitry.dygalo <dadygalo@gmail.com>
2018-07-28 23:51:01 +0200

    Add check for old SQLite version

---
+++ tests/test_sqlite.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+import pytest
+
+from xdump.sqlite import SQLiteBackend
+
+
+pytestmark = [pytest.mark.sqlite]
+
+
+@pytest.mark.skipif(sqlite3.sqlite_version_info >= (3, 8, 3), reason='Not relevant for newer SQLite version')
+def test_old_version_info():
+    with pytest.raises(RuntimeError):
+        SQLiteBackend()
