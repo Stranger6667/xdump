6574639ec14bd226f04f8825e9cb4023c401724d
Dmitry Dygalo <Stranger6667@users.noreply.github.com>
2018-08-01 23:03:18 +0200

    Simplify pg query (#46)
    * Drop query for CHECK constraints
    
    * Remove more
    
    * More cleanups
    
    * Change alias style, remove explicit type casts, remove redundant aliases
    
    * Make the query simpler if full_tables is empty
    
    * Fix

--- xdump/postgresql.py
+++ xdump/postgresql.py
@@ -14,113 +14,50 @@ from .utils import make_options
 SEQUENCES_SQL = "SELECT relname FROM pg_class WHERE relkind = 'S'"
 # The query below doesn't use `information_schema.table_constraints` and ``, but instead uses its modified versions
 # to mitigate permissions insufficiency on that views (they filter data by permissions of the current user)
+# Subqueries for constraints other than FOREIGN KEY are removed as well.
 BASE_RELATIONS_QUERY = '''
 SELECT
-    DISTINCT
-    tc.constraint_name, tc.table_name, kcu.column_name,
-    ccu.table_name AS foreign_table_name,
-    ccu.column_name AS foreign_column_name
+  DISTINCT
+  TC.constraint_name,
+  TC.table_name,
+  KCU.column_name,
+  CCU.foreign_table_name,
+  CCU.foreign_column_name
 FROM
-    (
+  (
     SELECT
-        c.conname AS constraint_name,
-        r.relname AS table_name,
-        CASE c.contype
-            WHEN 'c'::"char" THEN 'CHECK'::text
-            WHEN 'f'::"char" THEN 'FOREIGN KEY'::text
-            WHEN 'p'::"char" THEN 'PRIMARY KEY'::text
-            WHEN 'u'::"char" THEN 'UNIQUE'::text
-            ELSE NULL::text
-        END::information_schema.character_data AS constraint_type
-   FROM pg_namespace nc,
-    pg_namespace nr,
-    pg_constraint c,
-    pg_class r
-  WHERE
-    nc.oid = c.connamespace AND
-    nr.oid = r.relnamespace AND
-    c.conrelid = r.oid AND
-    (c.contype <> ALL (ARRAY['t'::"char", 'x'::"char"])) AND
-    r.relkind = 'r'::"char" AND NOT pg_is_other_temp_schema(nr.oid)
-UNION ALL
- SELECT
-    (
-      ((((nr.oid::text || '_'::text) || r.oid::text) || '_'::text) || a.attnum::text) || '_not_null'::text
-    ) AS constraint_name,
-    r.relname AS table_name,
-    'CHECK'::character varying::information_schema.character_data AS constraint_type
-   FROM pg_namespace nr,
-    pg_class r,
-    pg_attribute a
-  WHERE
-    nr.oid = r.relnamespace AND
-    r.oid = a.attrelid AND
-    a.attnotnull AND
-    a.attnum > 0 AND
-    NOT a.attisdropped AND
-    r.relkind = 'r'::"char" AND
-    NOT pg_is_other_temp_schema(nr.oid)
-    ) AS tc
-    JOIN information_schema.key_column_usage AS kcu
-      ON tc.constraint_name = kcu.constraint_name AND kcu.table_name = tc.table_name
+      CN.conname AS constraint_name,
+      CL.relname AS table_name
+    FROM pg_namespace NS,
+      pg_constraint CN,
+      pg_class CL
+    WHERE
+      NS.oid = CL.relnamespace AND
+      CN.conrelid = CL.oid AND
+      CN.contype = 'f' AND
+      CL.relkind = 'r' AND
+      NOT pg_is_other_temp_schema(NS.oid)
+    ) AS TC
+    JOIN information_schema.key_column_usage AS KCU
+      ON TC.constraint_name = KCU.constraint_name AND
+         KCU.table_name = TC.table_name
     JOIN (
-    SELECT
-    x.tblname AS table_name,
-    x.colname AS column_name,
-    x.cstrname AS constraint_name
-   FROM ( SELECT DISTINCT
-            r.relname,
-            a.attname,
-            c.conname
-           FROM pg_namespace nr,
-            pg_class r,
-            pg_attribute a,
-            pg_depend d,
-            pg_namespace nc,
-            pg_constraint c
-          WHERE
-            nr.oid = r.relnamespace AND
-            r.oid = a.attrelid AND
-            d.refclassid = 'pg_class'::regclass::oid AND
-            d.refobjid = r.oid AND
-            d.refobjsubid = a.attnum AND
-            d.classid = 'pg_constraint'::regclass::oid AND
-            d.objid = c.oid AND
-            c.connamespace = nc.oid AND
-            c.contype = 'c'::"char" AND
-            r.relkind = 'r'::"char" AND
-            NOT a.attisdropped
-        UNION ALL
-         SELECT
-            r.relname,
-            a.attname,
-            c.conname
-           FROM pg_namespace nr,
-            pg_class r,
-            pg_attribute a,
-            pg_namespace nc,
-            pg_constraint c
-          WHERE
-            nr.oid = r.relnamespace AND
-            r.oid = a.attrelid AND
-            nc.oid = c.connamespace AND
-            CASE
-              WHEN c.contype = 'f'::"char"
-                THEN
-                  r.oid = c.confrelid AND (a.attnum = ANY (c.confkey))
-                ELSE
-                  r.oid = c.conrelid AND (a.attnum = ANY (c.conkey))
-            END AND
-            NOT a.attisdropped AND
-            (
-              c.contype = ANY (ARRAY['p'::"char", 'u'::"char", 'f'::"char"])) AND
-              r.relkind = 'r'::"char"
-            ) x(tblname, colname, cstrname)
-    ) AS ccu
-      ON ccu.constraint_name = tc.constraint_name
-WHERE
-    constraint_type = 'FOREIGN KEY' AND
-    NOT(ccu.table_name = ANY(%(full_tables)s))
+      SELECT
+        CL.relname AS foreign_table_name,
+        AT.attname AS foreign_column_name,
+        CN.conname AS constraint_name
+      FROM pg_class CL,
+        pg_attribute AT,
+        pg_constraint CN
+      WHERE
+        CL.oid = AT.attrelid AND
+        CL.oid = CN.confrelid AND
+        AT.attnum = ANY (CN.confkey) AND
+        NOT AT.attisdropped AND
+        CN.contype = 'f' AND
+        CL.relkind = 'r'
+      ) AS CCU
+    ON CCU.constraint_name = TC.constraint_name
 '''
 
 
@@ -205,7 +142,13 @@ class PostgreSQLBackend(BaseBackend):
         file.writestr(self.sequences_filename, sequences)
 
     def add_related_data(self, full_tables, partial_tables):
-        self._related_data = self.run(BASE_RELATIONS_QUERY, {'full_tables': list(full_tables)})
+        if full_tables:
+            query = BASE_RELATIONS_QUERY + ' WHERE NOT(CCU.foreign_table_name = ANY(%(full_tables)s))'
+            kwargs = {'full_tables': list(full_tables)}
+        else:
+            query = BASE_RELATIONS_QUERY
+            kwargs = {}
+        self._related_data = self.run(query, kwargs)
         super(PostgreSQLBackend, self).add_related_data(full_tables, partial_tables)
 
     def get_foreign_keys(self, table, full_tables=(), recursive=False):
