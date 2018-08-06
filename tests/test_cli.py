927a5a6e3760de51c7b9b8e21b3aea677abf5390
dmitry.dygalo <dadygalo@gmail.com>
2018-08-06 21:05:59 +0200

    partial tables specification

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,14 +2,15 @@ import zipfile
 
 import pytest
 
-from xdump.cli import postgres
+from xdump import __version__
+from xdump.cli import postgres, xdump
 
 
 @pytest.fixture
 def xdump_pg(isolated_cli_runner, dsn_parameters, archive_filename):
 
     def runner(*args):
-        result = isolated_cli_runner.invoke(
+        return isolated_cli_runner.invoke(
             postgres,
             (
                 '-U', dsn_parameters['user'],
@@ -20,16 +21,22 @@ def xdump_pg(isolated_cli_runner, dsn_parameters, archive_filename):
             ) + args,
             catch_exceptions=False
         )
-        assert not result.exception
-        return result
 
     return runner
+
+
+def test_xdump_run(isolated_cli_runner):
+    """Smoke test for a click group."""
+    result = isolated_cli_runner.invoke(xdump, ('--version', ))
+    assert not result.exception
+    assert result.output == 'xdump, version {0}\n'.format(__version__)
 
 
 @pytest.mark.postgres
 @pytest.mark.usefixtures('schema', 'data')
 def test_xdump_postgres(xdump_pg, archive_filename, db_helper):
     result = xdump_pg('-f', 'groups')
+    assert not result.exception
     assert result.output == 'Dumping ...\nOutput file: {0}\nDone!\n'.format(archive_filename)
     archive = zipfile.ZipFile(archive_filename)
     db_helper.assert_groups(archive)
@@ -38,7 +45,46 @@ def test_xdump_postgres(xdump_pg, archive_filename, db_helper):
 @pytest.mark.postgres
 @pytest.mark.usefixtures('schema', 'data')
 def test_xdump_postgres_multiple_full_tables(xdump_pg, archive_filename, db_helper):
-    xdump_pg('-f', 'groups', '-f' 'tickets')
+    result = xdump_pg('-f', 'groups', '-f' 'tickets')
+    assert not result.exception
     archive = zipfile.ZipFile(archive_filename)
     db_helper.assert_groups(archive)
-    assert db_helper.get_tickets_count() == 5
+    db_helper.assert_content(
+        archive,
+        'tickets',
+        {
+            b'id,author_id,subject,message',
+            b'1,1,Sub 1,Message 1',
+            b'2,2,Sub 2,Message 2',
+            b'3,2,Sub 3,Message 3',
+            b'4,2,Sub 4,Message 4',
+            b'5,3,Sub 5,Message 5',
+        }
+    )
+
+
+@pytest.mark.postgres
+@pytest.mark.usefixtures('schema', 'data')
+def test_xdump_postgres_partial_tables(xdump_pg, archive_filename, db_helper):
+    result = xdump_pg('-p', 'employees:SELECT * FROM employees WHERE id = 1')
+    assert not result.exception
+    archive = zipfile.ZipFile(archive_filename)
+    db_helper.assert_content(archive, 'groups', {b'id,name', b'1,Admin'})
+    db_helper.assert_content(
+        archive,
+        'employees',
+        {
+            b'id,first_name,last_name,manager_id,referrer_id,group_id',
+            b'1,John,Doe,,,1',
+        }
+    )
+
+
+@pytest.mark.postgres
+@pytest.mark.usefixtures('schema', 'data')
+def test_xdump_postgres_partial_tables_invalid(xdump_pg):
+    result = xdump_pg('-p', 'shit')
+    assert result.exception
+    assert result.output == 'Usage: postgres [OPTIONS]\n\nError: ' \
+                            'Invalid value: partial table specification should be in the ' \
+                            'following format: "table:select SQL"\n'
--- xdump/cli.py
+++ xdump/cli.py
@@ -7,6 +7,20 @@ def xdump():
     pass
 
 
+def parse_partial(*partials):
+
+    def parse_value(value):
+        try:
+            table_name, sql = value.split(':', 1)
+            return table_name.strip(), sql.strip()
+        except ValueError:
+            raise click.exceptions.BadParameter(
+                'partial table specification should be in the following format: "table:select SQL"',
+            )
+
+    return dict(parse_value(partial) for partial in partials)
+
+
 @xdump.command()
 @click.option('-U', '--user', required=True, help='connect as specified database user')
 @click.option('-W', '--password', help='password for the DB connection')
@@ -15,12 +29,20 @@ def xdump():
 @click.option('-D', '--dbname', required=True, help='database to dump')
 @click.option('-o', '--output', required=True, help='output file name')
 @click.option('-f', '--full', help='table name to be fully dumped. Could be used multiple times', multiple=True)
-def postgres(user, password, host, port, dbname, output, full):
+@click.option(
+    '-p', '--partial',
+    help='partial tables specification in a form "table_name:select SQL". Could be used multiple times',
+    multiple=True
+)
+def postgres(user, password, host, port, dbname, output, full, partial):
+    if partial:
+        partial = parse_partial(*partial)
+
     click.echo('Dumping ...')
     click.echo('Output file: {0}'.format(output))
 
     from .postgresql import PostgreSQLBackend
 
     backend = PostgreSQLBackend(dbname=dbname, host=host, port=port, user=user, password=password)
-    backend.dump(output, full_tables=full)
+    backend.dump(output, full_tables=full, partial_tables=partial)
     click.echo('Done!')
