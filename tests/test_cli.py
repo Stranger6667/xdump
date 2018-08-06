91a152b2d2e97eb3a5722992c2513efaa2767120
dmitry.dygalo <dadygalo@gmail.com>
2018-08-06 15:05:50 +0200

    Test for multiple full tables

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,25 +5,42 @@ import pytest
 from xdump.cli import postgres
 
 
-@pytest.mark.postgres
-@pytest.mark.usefixtures('schema', 'data')
-def test_xdump_postgres(request, isolated_cli_runner, archive_filename, db_helper):
+@pytest.fixture
+def xdump_pg(request, isolated_cli_runner, archive_filename):
     postgresql = request.getfixturevalue('postgresql')
     parameters = postgresql.get_dsn_parameters()
 
-    result = isolated_cli_runner.invoke(
-        postgres,
-        (
-            '-U', parameters['user'],
-            '-H', parameters['host'],
-            '-P', parameters['port'],
-            '-D', parameters['dbname'],
-            '-o', archive_filename,
-            '-f', 'groups'
-        ),
-        catch_exceptions=False
-    )
-    assert not result.exception
+    def runner(*args):
+        result = isolated_cli_runner.invoke(
+            postgres,
+            (
+                '-U', parameters['user'],
+                '-H', parameters['host'],
+                '-P', parameters['port'],
+                '-D', parameters['dbname'],
+                '-o', archive_filename,
+            ) + args,
+            catch_exceptions=False
+        )
+        assert not result.exception
+        return result
+
+    return runner
+
+
+@pytest.mark.postgres
+@pytest.mark.usefixtures('schema', 'data')
+def test_xdump_postgres(xdump_pg, archive_filename, db_helper):
+    result = xdump_pg('-f', 'groups')
     assert result.output == 'Dumping ...\nOutput file: {0}\nDone!\n'.format(archive_filename)
     archive = zipfile.ZipFile(archive_filename)
     db_helper.assert_groups(archive)
+
+
+@pytest.mark.postgres
+@pytest.mark.usefixtures('schema', 'data')
+def test_xdump_postgres_multiple_full_tables(xdump_pg, archive_filename, db_helper):
+    xdump_pg('-f', 'groups', '-f' 'tickets')
+    archive = zipfile.ZipFile(archive_filename)
+    db_helper.assert_groups(archive)
+    assert db_helper.get_tickets_count() == 5
